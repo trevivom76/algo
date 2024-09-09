6a780b4090b0a350ff94a0628d896f84aa748647
Tae Sungwon <trevivom76@gmail.com>
2024-09-09 11:52:03 +0900

    [D3] Title: 증가하는 사탕 수열, Time: 127 ms, Memory: 45,048 KB -BaekjoonHub

--- SWEA/D3/20551. 증가하는 사탕 수열/증가하는 사탕 수열.py
+++ SWEA/D3/20551. 증가하는 사탕 수열/증가하는 사탕 수열.py
@@ -1,21 +1,20 @@
 T = int(input())
 
-for test_case in range(1, T + 1):
-    answer = 0
-    a, b, c = map(int, input().split())
+for tc in range(1, T + 1):
+    A, B, C = map(int, input().split()) # 굳이 리스트 필요 x
 
-    if c <= b:
-        # 조율 한다고 먹은거
-        answer += b - c + 1
-        # 사탕을 먹고 남은거
-        b = b - (b - c + 1)
+    if B < 2 or C < 3:       # A<B<C 구조를 만들 수 없는 케이스를 처리
+        print(f'#{tc} -1')
+        continue
 
-    if b <= a:
-        answer += a - b + 1
-        a = a - (a - b + 1)
+    eat = 0 # 먹은 사탕 개수
+    # 설계: B가 c 이상일 때, b = c - 1 이라면 최소 개수
+    if B >= C:
+        eat += B - (C - 1)      # 차이 만큼 먹는다.
+        B = C - 1
+    #       A 가 B 이상일 때, A = B - 1 이라면 최소 개수
+    if A >= B:
+        eat += A - (B - 1)
+        A = B - 1
 
-    #가능한 방법이 아닐 경우
-    if a <= 0 or b <= 0 or c <= 0:
-        answer = -1
-
-    print(f"#{test_case}", answer)
+    print(f'#{tc} {eat}')
